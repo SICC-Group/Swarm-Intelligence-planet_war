19cedd624b4279423b4a8c778552321403e90377
WuHang <2579516985@qq.com>
2020-03-28 09:17:35 +0800

    Update api.py

--- MAMRMT/example/api.py
+++ MAMRMT/example/api.py
@@ -1,15 +1,16 @@
-import random
 from MAMRMT.frame.builtin.env import *
 from MAMRMT.frame.Test import *
-from MAMRMT.frame.environment import *
+from frame.blackboard import *
+import pygame
+
 if __name__ == "__main__":
     # init the game
     env = environment(800, 600)
     # init models
     test = Test(ContractNetFactory())
     # init env and agents
-    list = [0, 1, 2]
-    index = random.randint(0, 2)
+    list = [1, 2, 3]
+    index = random.randint(1,3)
     mngAgent = test.getManager(index)
     agent={}
     exeAgent1 = test.getExecute(list[index - 1])
@@ -17,11 +18,25 @@ if __name__ == "__main__":
     exeAgent2 = test.getExecute(list[index - 2])
     agent[list[index - 2]]=exeAgent2
     env.updateEnv(mngAgent,agent)
-    # get group handles
+    # init blackboard
+    blackboard=blackboard()
+    blackboard.register(exeAgent1)
+    blackboard.register(exeAgent2)
+    # main loop
+    running=True
+    while running:
+        #get observation
+        data=env.get_observation(mngAgent,agent)
+        if data==False:
+            break
+        else:
+           task=mngAgent.assignTask(data)
+           # set action
+           blackboard.notify(task)
+           # get score
+           env.getScore(agent)
+        if(env.get_hitEnemies()>50):
+            break
+        pygame.display.flip()
 
-    # take action
-    # simulate oe step
-    # get reward
-    # clear dead agents
-    # reach max number of cycles
-    pass
+
