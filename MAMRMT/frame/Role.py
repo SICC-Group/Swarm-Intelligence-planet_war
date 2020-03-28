af2d85859edfe9316f62d0cf76b4654dc9e6e9d7
WuHang <2579516985@qq.com>
2020-03-28 09:46:07 +0800

    Update Role.py

--- MAMRMT/frame/Role.py
+++ MAMRMT/frame/Role.py
@@ -1,4 +1,3 @@
-import random
 import pygame
 from pygame.locals import *
 
@@ -7,29 +6,6 @@ class Role(pygame.sprite.Sprite):
         super(Role,self).__init__()
         self.roleId = roleId
 
-    def update(self, action,size):
-        if action == 0:
-            self.rect.move_ip(0, 0)
-        if action == 1:
-            self.rect.move_ip(0, -10)
-        if action == 2:
-            self.rect.move_ip(0, 10)
-        if action == 3:
-            self.rect.move_ip(-10, 0)
-        if action == 4:
-            self.rect.move_ip(10, 0)
-        # 限制plane的移动范围，不能超出屏幕
-        if self.rect.left < 0:
-            self.rect.left = 0
-        elif self.rect.right > size[0]:
-            self.rect.right = size[0]
-        if self.rect.top < 0:
-            self.rect.top = 0
-        elif self.rect.bottom > size[1]:
-            self.rect.bottom = size[1]
-
-    def run(self):
-        pass
 
 class ManagerAgent(Role):
     def __init__(self, roleId):
@@ -37,9 +13,30 @@ class ManagerAgent(Role):
         image = pygame.image.load('../source/GodPlane.png').convert()  # convert 转换像素格式
         self.image = pygame.transform.scale(image, (64, 64))  # transform 对图像翻转缩放旋转
         self.image.set_colorkey((255, 255, 255), RLEACCEL)  # 设置飞机透明度
-        self.rect = self.image.get_rect(center=(200, 500))  # set the plane's init position
-        self.obs_size = 100
+        self.rect = self.image.get_rect(center=(400, 400))  # set the plane's init position
+        self.obs_size = 400
 
+    def doTask(self,task,enemy):
+        m_obs = pygame.Rect(0, 0, 0, 0)
+        m_obs = self.rect.copy()
+        # 原位置放大后的矩形
+        m_obs.inflate_ip(self.obs_size,self.obs_size)
+        return task.doTask(m_obs,enemy)
+
+    def assignTask(self, data):
+        task = {}
+        # 判断exeAgent观测的敌机
+        for key in data[0].keys():
+            if (key != 'm'):
+                if (len(data[1]) == 0):
+                    task[key] = 0
+                elif (data[0][key][1] <= data[1][0][1]):
+                    del (data[1][0])
+                    task[key] = 0
+                else:
+                    task[key] = data[1][0][0] - data[0][key][0]
+                    del (data[1][0])
+        return task
 
 class ExecuteAgent(Role):
     def __init__(self, roleId):
@@ -47,18 +44,31 @@ class ExecuteAgent(Role):
         image = pygame.image.load('../source/mngPlane.png').convert()  # convert 转换像素格式
         self.image = pygame.transform.scale(image, (64, 64))  # transform 对图像翻转缩放旋转
         self.image.set_colorkey((255, 255, 255), RLEACCEL)  # 设置飞机透明度
-        self.rect = self.image.get_rect(center=(400, 400))  # set the plane's init position
-        self.obs_size = 20
+        self.rect = self.image.get_rect(center=(200+self.roleId*100,550))  # set the plane's init position
+        self.obs_size = 100
 
-    def execute(self):
+    def doTask(self,task,enemy):
+        e_ob = pygame.Rect(0, 0, 0, 0)
+        e_ob = self.rect.copy()
+        e_ob.inflate_ip(self.obs_size,self.obs_size)
+        return task.doTask(e_ob,enemy)
+
+    def execute(self, distance, action):
         pass
+
 
 class ManagerContractNet(ManagerAgent):
     print("ContractNet")
 
+
 class ExecuteContractNet(ExecuteAgent):
-    def execute(self):
-        print("Agent"+str(self.roleId)+"ExecuteContractNet")
+    def execute(self, distance, action):
+        if action == 0:
+            self.rect.move_ip(0, 0)
+        elif action == 1:
+            self.rect.move_ip(0, distance)
+        elif action == 2:
+            self.rect.move_ip(distance, 0)
 
 '''遗传算法'''
 class ManagerGenetic(ManagerAgent):
